3e8283d254aed32bc20d9469e66d3a446e12c876
jongisli <jongisli@gmail.com>
2013-11-14 14:51:24 +0100

    Implemented the Gun class to use with Box

---
+++ assignment4/djava/Gun.java
@@ -0,0 +1,27 @@
+package assignment4.djava;
+
+public class Gun {
+
+	private int ammo;
+	
+	public Gun(int ammo) {
+		this.ammo = ammo;
+	}
+
+	public void shoot() {
+		ammo--;
+	}
+	
+	public void shoot(int n)
+	{
+		while (n > 0) {
+			shoot();
+			n--;
+		}
+	}
+
+	public int getAmmoCount() {
+		return ammo;
+	}
+
+}
---
+++ assignment4/djava/tests/GunTest.java
@@ -0,0 +1,26 @@
+package assignment4.djava.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import assignment4.djava.Gun;
+
+public class GunTest {
+
+	@Test
+	public void ShootGunDecreasesAmmo() {
+		Gun gun = new Gun(12);
+		gun.shoot();
+		assertEquals(gun.getAmmoCount(), 11);
+	}
+	
+	@Test
+	public void EmptyClip()
+	{
+		Gun gun = new Gun(10);
+		gun.shoot(10);
+		assertEquals(gun.getAmmoCount(), 0);
+	}
+
+}
